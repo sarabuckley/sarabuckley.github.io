1d0654391099a03358dbdf1a128e94fdec26c2bb
sarabuckley <sara.buckley@live.com>
2018-11-05 15:54:50 -0800

    Add CSS styling to game

--- TriviaGame/assets/javascript/game.js
+++ TriviaGame/assets/javascript/game.js
@@ -6,6 +6,9 @@
   var pause = 0;
   var timesUp = false;
   var currentQuestion = 0;
+  var correctAnswer = 0;
+  var incorrectAnswer = 0;
+  var unAnswered = 0;
   var triviaObj = {};
   var messages = {
     correct: "Correct!",
@@ -16,9 +19,33 @@
          
 //Functions
 
+    function newGame(){
+
+        tempAnswers = []; 
+        seconds = 0;
+        time = "";
+        pause = 0;
+        timesUp = false;
+        currentQuestion = 0;
+        correctAnswer = 0;
+        incorrectAnswer = 0;
+        unAnswered = 0;
+        $("#message").empty();
+        $("#correctAnswers").empty();
+        $("#incorrectAnswers").empty();
+        $("#unanswered").empty();
+        $("#startOverBtn").empty();
+        $(".triviaAnswerBtns").empty();
+        $("#answer").empty();
+        $("#startBtn").hide();
+        $('#startOverBtn').hide();
+
+        triviaQuery();         
+    }
+
     function triviaQuery() {
          
-         var queryURL = "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple";
+         var queryURL = "https://opentdb.com/api.php?amount=5&category=22&type=multiple";
             
          $.ajax({
                 url: queryURL,
@@ -39,7 +66,8 @@
             
             if (currentQuestion < triviaObj.results.length)
             {
-                $("#triviaQuestion").html(triviaObj.results[currentQuestion].question);
+                $("#triviaQuestion").html("<br>" + triviaObj.results[currentQuestion].question + "<br>");
+                $("#triviaQuestion").show();
                 tempAnswers = [];
 
                 for (var j = 0; j < triviaObj.results[currentQuestion].incorrect_answers.length; j++ ){ 
@@ -50,6 +78,8 @@
 
                 // Dynamically generate buttons for multiple choice answers
                 for (var k = 0; k < tempAnswers.length; k++){
+                    var b = $("<br>");
+                    $(".triviaAnswerBtns").append(b);
                     var a = $("<button>");
                     a.addClass("triviaBtn");
                     a.attr("data-answer", tempAnswers[k]);
@@ -57,7 +87,7 @@
                     $(".triviaAnswerBtns").append(a);
                 }
                 // Wait 30 seconds for answer
-            startCountdown();
+                startCountdown();
               }                    
             
     }
@@ -72,19 +102,18 @@
     }      
 
     function startCountdown(){
-          seconds = 30;
-          $("#timer").html("Time Remaining: " + seconds + " seconds");
-          //sets timer to go down
+          
+          seconds = 20;
+          $("#timer").show();
+          $("#timer").html("Seconds Remaining: " + seconds);
           time = setInterval(showCountdown, 1000);
     }
 
     function showCountdown() {
-          
-          timesUp = false;
-          seconds--;
-          $("#timer").html("Time Remaining: " + seconds + " seconds");
-//          $("#timer").css("font-size", seconds + 8 + "px");
 
+          timesUp = false;
+          seconds--;          
+          $("#timer").html("Seconds Remaining: " + seconds);
           if (seconds === 0 || seconds < 0) {
               timesUp = true;            
               answerCheck();          
@@ -95,27 +124,53 @@
 
           clearInterval(time);
           clearTimeout(pause);
+          $("#timer").hide();
+          $(".triviaAnswerBtns").empty();
+          $("#triviaQuestion").hide();
 
           if ($(this).attr("data-answer") === triviaObj.results[currentQuestion].correct_answer) {
               clearInterval(time);
+              correctAnswer ++;
               $("#message").html(messages.correct);
           } else if (timesUp){
+              unAnswered++;
               $("#message").html(messages.endTime);
               $("#answer").html("The correct answer is: " + triviaObj.results[currentQuestion].correct_answer);
           } else {
+              incorrectAnswer++;
               $("#message").html(messages.incorrect);
               $("#answer").html("The correct answer is: " + triviaObj.results[currentQuestion].correct_answer);    
           }
 
           currentQuestion++;
-          pause = setTimeout(triviaQuestions, 3000);
+
+          // check if end of game
+          if (currentQuestion > (triviaObj.results.length - 1)) {
+                setTimeout(scoreGame, 3000);
+          } else {
+                setTimeout(triviaQuestions, 3000);
+          }
       }
+
+    function scoreGame(){
+        $("#timer").empty();
+        $("#triviaQuestion").empty();
+        $(".triviaAnswerBtns").empty();
+        $("#answer").empty();
+        $("#message").html("<br>" + messages.finished + "<br>");
+        $("#correctAnswers").html("<br> Correct Answers: " + correctAnswer + "<br>");
+        $("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswer);
+        $("#unanswered").html("Unanswered: " + unAnswered);
+	    $('#startOverBtn').show();
+	    $('#startOverBtn').html('Play Again?');
+    }
+
 
 //Click Events
 
-  $("#startGame").on("click", triviaQuery);
+  $("#startBtn").on("click", newGame);
+  $("#startOverBtn").on("click", newGame);
   $(document).on("click", ".triviaBtn", answerCheck);
+  $('#startOverBtn').hide();
 
 
-//  $(document).on("mouseover", ".crystal-image", mouseoverCrystals);
-//  $(document).on("mouseout", ".crystal-image-mouseOver", mouseoutCrystals);
